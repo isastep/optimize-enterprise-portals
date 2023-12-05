33902679618d21158a95765f3545880e8f5f3187
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2023-12-05 22:35:58 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,26 @@
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+        print(f"Random Number: {item}")
+
+    for item in data:
+def generate_random_data():
+    data = generate_random_data()
+    return data
+if __name__ == "__main__":
+    main()
+
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+    }
+        }
+
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+
+        }
